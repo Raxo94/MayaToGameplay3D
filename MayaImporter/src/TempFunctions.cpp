92cb5892b3279e216c60a1228ab2d3d2f40ed94e
unknown <BTH@H473-0009.students.ad.bth.se>
2016-10-11 13:26:30 +0200

    Moved Camera To function. There is still an issue with proj matrix. But with reset proj func it works

---
+++ MayaImporter/src/TempFunctions.cpp
@@ -0,0 +1,41 @@
+#include "gameplay.h"
+
+using namespace::gameplay;
+ 
+
+inline Node* getManualCamera()
+{
+	Node* cameraNode = Node::create("Camera");
+
+	//_scene->addNode(cameraNode);
+
+	cameraNode->setTranslationX(1.5);
+	cameraNode->setTranslationY(2.3);
+	cameraNode->setTranslationZ(7.2);
+
+	//Matrix roationMatrix;
+	//Quaternion Test(-0.142, 0.126, 0, 1);
+	//Matrix::createRotation(Test, &roationMatrix);
+	//cameraNode->setRotation(roationMatrix);
+
+	Camera* cam = cameraNode->getCamera();
+
+	cam = Camera::createPerspective(0, 0, 0, 0);
+
+	float camMatrix[4][4];
+	Matrix projectionMatrix(camMatrix[0][0], camMatrix[1][0], camMatrix[2][0], camMatrix[3][0],
+		camMatrix[0][1], camMatrix[1][1], camMatrix[2][1], camMatrix[3][1],
+		camMatrix[0][2], camMatrix[1][2], -camMatrix[2][2], -camMatrix[3][2],
+		camMatrix[0][3], camMatrix[1][3], camMatrix[2][3], camMatrix[3][3]);
+
+	cam->setProjectionMatrix(projectionMatrix);
+	cam->resetProjectionMatrix();
+
+
+	cam->setNearPlane(0.25);
+	cam->setFarPlane(100);
+	cam->setFieldOfView(27);
+
+	cameraNode->setCamera(cam);
+	return cameraNode;
+}
